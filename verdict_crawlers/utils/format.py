8211b0bc51fd0d4c6ab139743b5951e74ab725a9
hsinyilin <985471zxv@gmail.com>
2023-04-19 09:42:01 +0800

    delete format.py

--- verdict_crawlers/utils/format.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-
-def roc_to_ad(roc_date_str):
-    roc_year, roc_month, roc_day = map(int, roc_date_str.split("."))
-    ad_year = roc_year + 1911
-    ad_date = date(ad_year, roc_month, roc_day)
-    return ad_date

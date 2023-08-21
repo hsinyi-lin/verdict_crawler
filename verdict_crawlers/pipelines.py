0768063c51a1d3f956a05e94e0f7db592132a4b1
hsinyilin <985471zxv@gmail.com>
2023-08-21 12:14:14 +0800

    update spider logger info

--- verdict_crawlers/pipelines.py
+++ verdict_crawlers/pipelines.py
@@ -74,29 +74,29 @@ class MySQLPipeline:
 
         crime_list = [None, '竊盜罪', '殺人罪', '強盜罪', '酒駕致死罪']
 
-        print('爬蟲抓取結果.....')
-        print(item)
+        spider.logger.info('爬蟲抓取結果.....')
+        spider.logger.info(item)
 
-        print(f"此篇為關於「{crime_list[item['crime_id']]}」 相關之判例")
-        print('ChatGPT已根據判決書內文，提取以下.....')
-        print(f'自動產生標題: {title}')
-        print(f'自動產生摘要及將專業用語轉換成大眾敘述: {incident_lite}')
+        spider.logger.info(f"此篇為關於「{crime_list[item['crime_id']]}」 相關之判例")
+        spider.logger.info('ChatGPT已根據判決書內文，提取以下.....')
+        spider.logger.info(f'自動產生標題: {title}')
+        spider.logger.info(f'自動產生摘要及將專業用語轉換成大眾敘述: {incident_lite}')
 
         chinese_list = ['是' if feature else '否' for feature in features]
 
         if item['crime_id'] == 1:
-            print('提取特徵：\n(1)偷竊物是否與金錢相關 (2)是否遺棄贓物 (3)犯罪地點是否在室內 (4)竊盜方法是否具破壞性 (5)是否兩人(含)以上犯罪 (6)是否使用交通工具來輸送贓物 (7)是否有前科 (8)是否竊取之財物為被害人生財工具')
-            print('提取結果:', chinese_list)
-            print(f'提取判刑年月數：大約會被判刑 {prison_term}個月')
+            spider.logger.info('提取特徵：\n(1)偷竊物是否與金錢相關 (2)是否遺棄贓物 (3)犯罪地點是否在室內 (4)竊盜方法是否具破壞性 (5)是否兩人(含)以上犯罪 (6)是否使用交通工具來輸送贓物 (7)是否有前科 (8)是否竊取之財物為被害人生財工具')
+            spider.logger.info('提取結果:', chinese_list)
+            spider.logger.info(f'提取判刑年月數：大約會被判刑 {prison_term}個月')
         elif item['crime_id'] == 2:
             pass
         elif item['crime_id'] == 3:
-            print('提取特徵：(1)是否導致受害者受傷 (2)是否兩人(含)以上犯案 (3)是否使用刀械 (4)是否擁有前科 (5)是否有計畫犯罪 (6)是否被害人人數超過兩人(含) (7)是否提及犯案人因生活困境而強盜 (8)是否毀損物品或建築')
-            print('提取結果:', chinese_list)
-            print(f'提取判刑年月數：大約會被判刑 {prison_term[0]} 年又 {prison_term[1]} 個月，總月數 {prison_term[2]} 個月')
+            spider.logger.info('提取特徵：(1)是否導致受害者受傷 (2)是否兩人(含)以上犯案 (3)是否使用刀械 (4)是否擁有前科 (5)是否有計畫犯罪 (6)是否被害人人數超過兩人(含) (7)是否提及犯案人因生活困境而強盜 (8)是否毀損物品或建築')
+            spider.logger.info('提取結果:', chinese_list)
+            spider.logger.info(f'提取判刑年月數：大約會被判刑 {prison_term[0]} 年又 {prison_term[1]} 個月，總月數 {prison_term[2]} 個月')
         elif item['crime_id'] == 4:
             pass
         
-        print(f'----------{new_id}-END----------')
+        spider.logger.info(f'----------{new_id}-END----------')
 
         return item
